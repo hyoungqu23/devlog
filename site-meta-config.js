d5e2faea756e52119c59c0476e0beaf1f90e4565
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-04-09 23:20:03 +0900

    initial push

---
+++ site-meta-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  title: `HyoungMin's minilog`,
+  subtitle: `Junior Front-End Developers Quenching Every Day`,
+  description: `Harden and sharpen skills every day and every night.`,
+  author: {
+    name: `@HyoungMin`,
+    summary: `live in Seoul, Korea`,
+  },
+  introduction: `안녕하세요. 매일 단조하고 담금질하며 기술을 연마하는 주니어 프론트엔드 개발자 이형민입니다.`,
+  siteUrl: `https://hyoungqu23.github.io/`, // Your blog site url
+  social: {
+    github: `hyoungqu23@gmail.com`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: `hyoungqu23`,
+  },
+  icon: `src/images/profile-pic.png`, // Add your favicon
+  keywords: [
+    `blog`,
+    `gatsby`,
+    `application`,
+    `react`,
+    `javascript`,
+    `HTML`,
+    `CSS`,
+    `JAMStack`,
+    `HyoungMin`,
+    `FontEnd`,
+    `Developer`,
+    `HyoungMin's miniLog`,
+    `miniLog`,
+    `Develog`,
+  ],
+  comment: {
+    utterances: 'Ha-Young/my-blog-old', // Your repository for archive comment
+  },
+}
